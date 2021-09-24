adaac21f82214d5e85337e704132233b2e9a893b
Yuri Zhulev <yuri.zhulyov@gmail.com>
2021-09-24 14:18:26 +0300

    feat: finish popup

---
+++ scrips/index.js
@@ -0,0 +1,41 @@
+const title = document.querySelector('.profile__title');
+const subtitle = document.querySelector('.profile__subtitle');
+const editBtn = document.querySelector('.profile__edit');
+const popup = document.querySelector('.popup');
+const closeBtn = popup.querySelector('.close-btn');
+const nameInput = popup.querySelector('[name=name].popup__input-text');
+const subtitleInput = popup.querySelector('[name=subtitle].popup__input-text');
+const form = popup.querySelector('.popup__form');
+
+const openPopupHandler = () => {
+  popup.classList.add('popup_is-visible');
+  editFormDataHandler(title);
+};
+
+const closePopupHandler = () => {
+  popup.classList.remove('popup_is-visible');
+};
+
+const closeOverlayHandler = (e) => {
+  if (e.target === e.currentTarget) {
+    popup.classList.remove('popup_is-visible');
+  }
+};
+
+const editFormDataHandler = () => {
+  nameInput.value = title.textContent;
+  subtitleInput.value = subtitle.textContent;
+};
+
+const formSubmitHandler = (e) => {
+  e.preventDefault();
+  console.log(nameInput.value);
+  title.textContent = nameInput.value;
+  subtitle.textContent = subtitleInput.value;
+  closePopupHandler();
+};
+
+editBtn.addEventListener('click', openPopupHandler);
+closeBtn.addEventListener('click', closePopupHandler);
+popup.addEventListener('click', closeOverlayHandler);
+form.addEventListener('submit', formSubmitHandler);

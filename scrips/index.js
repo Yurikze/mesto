05745ba1350b620e4fc65140dde6e1f3b57bc0ed
Yuri Zhulev <yuri.zhulyov@gmail.com>
2021-10-08 10:49:26 +0300

    feat: init places in JS

--- scrips/index.js
+++ scrips/index.js
@@ -6,6 +6,46 @@ const closeBtn = popup.querySelector('.popup__close-btn');
 const nameInput = popup.querySelector('[name=name].popup__input-text');
 const subtitleInput = popup.querySelector('[name=subtitle].popup__input-text');
 const form = popup.querySelector('.popup__form');
+const placesList = document.querySelector('.places__list');
+const placeTemplate = document.querySelector('#place__li').content;
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg',
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg',
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg',
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg',
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
+  },
+];
+
+const renderPlaceItem = (item) => {
+  const placeElem = placeTemplate.querySelector('.places__li').cloneNode(true);
+  placeElem.querySelector('.places__img').src = item.link;
+  placeElem.querySelector('.places__img').alt = item.name;
+  placeElem.querySelector('.places__title').textContent = item.name;
+
+  placesList.append(placeElem);
+};
+
+initialCards.forEach(renderPlaceItem);
 
 const openPopupHandler = () => {
   popup.classList.add('popup_is-visible');

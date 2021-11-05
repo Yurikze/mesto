218200bf4895e3dcf8a03bf593653817438ca372
Yuri Zhulev <yuri.zhulyov@gmail.com>
2021-11-05 15:43:00 +0300

    refactor: card class

--- scrips/index.js
+++ scrips/index.js
@@ -1,3 +1,6 @@
+import { initialCards } from './dummy_data.js';
+import { Card } from './Card.js';
+
 const title = document.querySelector('.profile__title');
 const subtitle = document.querySelector('.profile__subtitle');
 const editBtn = document.querySelector('.profile__edit');
@@ -12,51 +15,20 @@ const addForm = popupAdd.querySelector('.popup__form');
 const placeTitleInput = popupAdd.querySelector('[name=title].popup__input');
 const placeImgSrcInput = popupAdd.querySelector('[name=subtitle].popup__input');
 
-const placeTemplate = document.querySelector('#place__li').content;
-const popupImg = document.querySelector('.popup-image');
-const popupImgSrc = popupImg.querySelector('.popup-image__img');
-const popupImgText = popupImg.querySelector('.popup-image__subtitle');
-const popups = document.querySelectorAll('.popup');
-
-const createCard = (item) => {
-  const placeElem = placeTemplate.querySelector('.places__li').cloneNode(true);
-  const placeImg = placeElem.querySelector('.places__img');
-  placeImg.src = item.link;
-  placeImg.alt = item.name;
-  placeElem.querySelector('.places__title').textContent = item.name;
-  placeElem
-    .querySelector('.places__like-btn')
-    .addEventListener('click', (e) => {
-      e.target.classList.toggle('places__like-btn_active');
-    });
-  placeElem
-    .querySelector('.places__delete-icon')
-    .addEventListener('click', (e) => {
-      e.target.closest('.places__li').remove();
-    });
-  placeImg.addEventListener('click', (e) => {
-    if (e.currentTarget === e.target) {
-      openPopupHandler(popupImg);
-      popupImgSrc.src = item.link;
-      popupImgSrc.alt = item.name;
-      popupImgText.textContent = item.name;
-    }
-  });
-  return placeElem;
-};
-
-const renderPlaceItem = (item) => {
-  const placeElem = createCard(item);
-  placesList.prepend(placeElem);
-};
-
-initialCards.forEach(renderPlaceItem);
-
 const openPopupHandler = (popup) => {
   popup.classList.add('popup_is-visible');
   document.addEventListener('keydown', onEscPopupHandler);
   document.addEventListener('click', onClosePopup);
 };
+
+const renderPlaceItem = (item) => {
+  const data = {...item, openPopupHandler}
+  const place = new Card(data, '#place__li');
+  const placeElem = place.generateCard();
+  placesList.prepend(placeElem);
+};
+
+initialCards.forEach(renderPlaceItem);
 
 const onEscPopupHandler = (e) => {
   onClosePopup(e);
---
+++ scrips/utils.js
@@ -0,0 +1,3 @@
+export const popupImg = document.querySelector('.popup-image');
+export const popupImgSrc = popupImg.querySelector('.popup-image__img');
+export const popupImgText = popupImg.querySelector('.popup-image__subtitle');
